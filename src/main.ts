d7d364fdfb29309d523b48a85a1c64de7d14174a
Dylan Vorster <dylanvorster.dv@gmail.com>
2017-12-06 22:27:18 +0200

    fix exports

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+export * from "./widgets/PanelWidget";
+export * from "./widgets/WorkspaceWidget";
+export * from "./widgets/BemWidget";
+export * from "./widgets/ContainerWidget";
+export * from "./widgets/DraggableWidget";
+export * from "./widgets/FloatingPanelWidget";
+export * from "./widgets/DropZoneWidget";
+export * from "./widgets/TabButtonWidget";
+export * from "./widgets/TabGroupWidget";
+
+export * from "./widgets/layouts/MicroLayoutWidget";
+export * from "./widgets/layouts/StandardLayoutWidget";
+
+export * from "./models/AbstractWorkspaceModel";
+export * from "./models/AbstractWorkspaceCollectionModel";
+export * from "./models/WorkspaceNodeModel";
+export * from "./models/WorkspacePanelModel";
+export * from "./models/WorkspaceTabbedModel";
