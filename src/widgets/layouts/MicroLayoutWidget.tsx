474af0e1100a35a84f0032ce113fce636264c74c
Dylan Vorster <dylanvorster.dv@gmail.com>
2020-02-19 14:47:53 +0200

    micro layout overflow

--- src/widgets/layouts/MicroLayoutWidget.tsx
+++ src/widgets/layouts/MicroLayoutWidget.tsx
@@ -17,8 +17,15 @@ export interface MicroLayoutWidgetProps {
 namespace S {
 	export const MicroLayout = styled.div`
 		display: flex;
-		flex-direction: column;
 		flex-grow: 1;
+		overflow: hidden;
+	`;
+
+	export const Scrollable = styled.div`
+		display: flex;
+		flex-direction: column;
+		overflow-y: scroll;
+		margin-right: -30px;
 	`;
 }
 
@@ -58,36 +65,38 @@ export class MicroLayoutWidget extends React.Component<MicroLayoutWidgetProps> {
 				ref={ref => {
 					this.div = ref;
 				}}>
-				{_.map(this.props.node.getFlattened(), child => {
-					let selected = this.props.node.floatingModel && this.props.node.floatingModel.id === child.id;
-					return (
-						<div
-							key={child.id}
-							ref={ref => {
-								this.buttons[child.id] = ref;
-							}}>
-							<DraggableWidget
-								onClick={() => {
-									if (selected) {
-										this.props.node.setFloatingModel(null);
-									} else {
-										this.props.node.setFloatingModel(child);
-									}
-									this.props.engine.fireRepaintListeners();
-								}}
-								engine={this.props.engine}
-								model={child}>
-								{this.props.engine.getFactory<WorkspacePanelFactory>(child).generateMicroButton({
-									model: child,
-									selected: selected,
-									engine: this.props.engine
-								})}
-							</DraggableWidget>
-						</div>
-					);
-				})}
-				{// is rendered into a react portal
-				this.props.node.floatingModel && this.getFloatingModel()}
+				<S.Scrollable>
+					{_.map(this.props.node.getFlattened(), child => {
+						let selected = this.props.node.floatingModel && this.props.node.floatingModel.id === child.id;
+						return (
+							<div
+								key={child.id}
+								ref={ref => {
+									this.buttons[child.id] = ref;
+								}}>
+								<DraggableWidget
+									onClick={() => {
+										if (selected) {
+											this.props.node.setFloatingModel(null);
+										} else {
+											this.props.node.setFloatingModel(child);
+										}
+										this.props.engine.fireRepaintListeners();
+									}}
+									engine={this.props.engine}
+									model={child}>
+									{this.props.engine.getFactory<WorkspacePanelFactory>(child).generateMicroButton({
+										model: child,
+										selected: selected,
+										engine: this.props.engine
+									})}
+								</DraggableWidget>
+							</div>
+						);
+					})}
+					{// is rendered into a react portal
+					this.props.node.floatingModel && this.getFloatingModel()}
+				</S.Scrollable>
 			</S.MicroLayout>
 		);
 	}
