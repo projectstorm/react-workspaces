50874c4d327925644c740b1632bef2afd43ac30e
Dylan Vorster <dylanvorster.dv@gmail.com>
2022-11-03 16:22:07 -0600

    remove some duplicate code

--- workspace-core/src/entities/tray/WorkspaceTrayModel.ts
+++ workspace-core/src/entities/tray/WorkspaceTrayModel.ts
@@ -1,8 +1,5 @@
-import { WorkspaceCollectionModel } from '../../core-models/WorkspaceCollectionModel';
 import { WorkspaceEngine } from '../../core/WorkspaceEngine';
 import { WorkspaceModel } from '../../core-models/WorkspaceModel';
-import { Alignment } from '../../core/tools';
-import { DimensionContainer } from '../../core/DimensionContainer';
 import { WorkspaceNodeModel } from '../node/WorkspaceNodeModel';
 
 export type WorkspaceNodeModelMode = 'expand' | 'micro';
@@ -13,7 +10,6 @@ export class WorkspaceTrayModel extends WorkspaceNodeModel {
 	floatingModel: WorkspaceModel;
 
 	static NAME = 'srw-tray';
-	r_divisons: DimensionContainer[];
 
 	constructor() {
 		super(WorkspaceTrayModel.NAME);
@@ -26,54 +22,20 @@ export class WorkspaceTrayModel extends WorkspaceNodeModel {
 	toArray() {
 		return {
 			...super.toArray(),
-			mode: this.mode,
-			vertical: this.vertical
+			mode: this.mode
 		};
-	}
-
-	recomputeDivisions() {
-		this.r_divisons = this.children
-			.map((c) => {
-				return new DimensionContainer();
-			})
-			.concat(new DimensionContainer());
-	}
-
-	addModel(model: WorkspaceModel, position: number = null): this {
-		super.addModel(model, position);
-		this.recomputeDivisions();
-		return this;
 	}
 
 	fromArray(payload: any, engine: WorkspaceEngine) {
 		super.fromArray(payload, engine);
-		this.vertical = payload['vertical'];
 		this.mode = payload['mode'];
 	}
 
 	removeModel(model: WorkspaceModel, runNormalizationChecks: boolean = true): this {
 		super.removeModel(model, runNormalizationChecks);
-		this.recomputeDivisions();
 		if (this.floatingModel && this.floatingModel === model) {
 			this.floatingModel = null;
 		}
-		return this;
-	}
-
-	shouldExpand() {
-		if (!this.vertical) {
-			return this.expandVertical;
-		}
-		return this.expandHorizontal;
-	}
-
-	setVertical(vertical: boolean = true): this {
-		this.vertical = vertical;
-		return this;
-	}
-
-	setHorizontal(horizontal: boolean = true): this {
-		this.vertical = !horizontal;
 		return this;
 	}
 
@@ -86,35 +48,5 @@ export class WorkspaceTrayModel extends WorkspaceNodeModel {
 	setFloatingModel(model: WorkspaceModel | null): this {
 		this.floatingModel = model;
 		return this;
-	}
-
-	getModelBefore(model: WorkspaceModel) {
-		const index = this.children.indexOf(model);
-		if (index <= 0) {
-			return null;
-		}
-		return this.children[index - 1];
-	}
-
-	getModelAfter(model: WorkspaceModel) {
-		const index = this.children.indexOf(model);
-		if (index >= this.children.length - 1) {
-			return null;
-		}
-		return this.children[index + 1];
-	}
-
-	getChildSibling(model: WorkspaceModel, alignment: Alignment): WorkspaceModel {
-		if (this.vertical && alignment === Alignment.TOP) {
-			return this.getModelBefore(model);
-		} else if (this.vertical && alignment === Alignment.BOTTOM) {
-			return this.getModelAfter(model);
-		}
-		if (!this.vertical && alignment === Alignment.LEFT) {
-			return this.getModelBefore(model);
-		} else if (!this.vertical && alignment === Alignment.RIGHT) {
-			return this.getModelAfter(model);
-		}
-		return null;
 	}
 }
